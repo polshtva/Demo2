6e71e2d376a6639a4093796465814774c2d77f0a
Polina Shestakova <polina-shestakova2003mail.ru>
2024-05-05 10:16:06 +0500

    Добавление роли менеджера

--- python.py
+++ python.py
@@ -3,6 +3,7 @@ from tkinter import *
 from tkinter import ttk, messagebox
 import pyodbc as py
 from PIL import Image, ImageTk
+from tkinter import Tk, Label, Entry, OptionMenu, StringVar, Button, messagebox
 
 
 def closeWindow(window):
@@ -32,25 +33,25 @@ def open_windowTask(user_id):
     
     status_var = "В ожидании"
 
-    btn_save = Button(new_windowTask, text='Отправить заявку', command=lambda: save_and_update_treeview(current_date, equipment_entry, type_server_entry, desc_server_entry, user_id, status_var,new_windowTask))
+    employ_data = "Поиск работника"
+    btn_save = Button(new_windowTask, text='Отправить заявку', command=lambda: save_and_update_treeview(current_date, equipment_entry, type_server_entry, desc_server_entry, user_id, status_var, employ_data, new_windowTask))
     btn_save.pack()
     
     btn_close = Button(new_windowTask, text='Закрыть', command=lambda: closeWindow(new_windowTask))
     btn_close.pack()
 
-def save_and_update_treeview(current_date, equipment_entry, type_server_entry, desc_server_entry, user_id, status,new_windowTask):
-    writeDataTask(current_date, equipment_entry.get(), type_server_entry.get(), desc_server_entry.get("1.0", "end-1c"), user_id, status)
+def save_and_update_treeview(current_date, equipment_entry, type_server_entry, desc_server_entry, user_id, status, employ_data, new_windowTask):
+    writeDataTask(current_date, equipment_entry.get(), type_server_entry.get(), desc_server_entry.get("1.0", "end-1c"), user_id, status, employ_data)
     closeWindow(new_windowTask)
 
-def writeDataTask(data, equipment, type, desc, user_id, status):
+def writeDataTask(data, equipment, type, desc, user_id, status, employ_data):
     connectionStr = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=.;DATABASE=Account;Trusted_Connection=yes'
     connection = py.connect(connectionStr)
     dbCursor = connection.cursor()
-    dbCursor.execute("INSERT INTO Servers (Data_Server, Equipment, Type_Server, Desc_Server, Id_Client, Status_Server) VALUES (?, ?, ?, ?, ?, ?)", (data, equipment, type, desc, user_id, status))
+    dbCursor.execute("INSERT INTO Servers (Data_Server, Equipment, Type_Server, Desc_Server, Id_Client, Status_Server, Employ_Data) VALUES (?, ?, ?, ?, ?, ?, ?)", (data, equipment, type, desc, user_id, status, employ_data))
     print("Данные добавились")
     connection.commit()
     connection.close()
-
 
 def open_userwindow(user_id):
     new_windowUser = Tk()
@@ -62,7 +63,7 @@ def open_userwindow(user_id):
     btn_save.pack()
     
     # Создание и настройка Treeview
-    tree = ttk.Treeview(new_windowUser, columns=("Id", "Date", "Equipment", "Type", "Description", "Client", "Status"), show="headings")
+    tree = ttk.Treeview(new_windowUser, columns=("Id", "Date", "Equipment", "Type", "Description", "Client", "Status", "Employ_Data"), show="headings")
     
     tree.column("#0", width=0, stretch="NO")
     tree.column("Id", anchor="center", width=50)
@@ -70,8 +71,9 @@ def open_userwindow(user_id):
     tree.column("Equipment", anchor="center", width=150)
     tree.column("Type", anchor="center", width=160)
     tree.column("Description", anchor="center", width=360)
-    tree.column("Client", anchor="center", width=360)
-    tree.column("Status", anchor="center", width=360)
+    tree.column("Client", anchor="center", width=50)
+    tree.column("Status", anchor="center", width=260)
+    tree.column("Employ_Data", anchor="center", width=260)
     
     tree.heading("Id", text="ID")
     tree.heading("Date", text="Дата заявки")
@@ -80,6 +82,7 @@ def open_userwindow(user_id):
     tree.heading("Description", text="Описание")
     tree.heading("Client", text="Id клиент")
     tree.heading("Status", text="Статус")
+    tree.heading("Employ_Data", text="Обслуживающий заявку")
     
     tree.pack(fill="both", expand=True)
 
@@ -87,7 +90,7 @@ def open_userwindow(user_id):
     connectionStr = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=.;DATABASE=Account;Trusted_Connection=yes'
     connection = py.connect(connectionStr)
     dbCursor = connection.cursor()
-    dbCursor.execute("SELECT Id_Server, Data_Server, Equipment, Type_Server, Desc_Server, Id_Client, Status_Server FROM Servers")
+    dbCursor.execute("SELECT Id_Server, Data_Server, Equipment, Type_Server, Desc_Server, Id_Client, Status_Server, Employ_Data FROM Servers")
     rows = dbCursor.fetchall()
 
     for row in rows:
@@ -100,9 +103,157 @@ def open_userwindow(user_id):
     
     tree.bind("<Double-1>", lambda event: open_request_window(event, tree))
 
+def open_menagerwindow(user_id):
+    new_windowMenager = Tk()
+    new_windowMenager.title("Окно менаджера")
+    new_windowMenager.attributes('-fullscreen',True)
+     # Добавление кнопки создания заявки
+    btn_save = Button(new_windowMenager, text='Создать заявку', command=lambda: open_windowTask(user_id))
+    btn_save.pack()
+    
+    # Создание и настройка Treeview
+    tree = ttk.Treeview(new_windowMenager, columns=("Id", "Date", "Equipment", "Type", "Description", "Client", "Status", "Employ_Data"), show="headings")
+    
+    tree.column("#0", width=0, stretch="NO")
+    tree.column("Id", anchor="center", width=50)
+    tree.column("Date", anchor="w", width=100)
+    tree.column("Equipment", anchor="center", width=150)
+    tree.column("Type", anchor="center", width=160)
+    tree.column("Description", anchor="center", width=360)
+    tree.column("Client", anchor="center", width=50)
+    tree.column("Status", anchor="center", width=260)
+    tree.column("Employ_Data", anchor="center", width=260)
+    
+    tree.heading("Id", text="ID")
+    tree.heading("Date", text="Дата заявки")
+    tree.heading("Equipment", text="Название оборудования")
+    tree.heading("Type", text="Тип")
+    tree.heading("Description", text="Описание")
+    tree.heading("Client", text="Id клиент")
+    tree.heading("Status", text="Статус")
+    tree.heading("Employ_Data", text="Обслуживающий заявку")
+    
+    tree.pack(fill="both", expand=True)
+
+    # Загрузка данных из базы данных и добавление только соответствующих user_id записей в Treeview
+    connectionStr = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=.;DATABASE=Account;Trusted_Connection=yes'
+    connection = py.connect(connectionStr)
+    dbCursor = connection.cursor()
+    dbCursor.execute("SELECT Id_Server, Data_Server, Equipment, Type_Server, Desc_Server, Id_Client, Status_Server, Employ_Data FROM Servers")
+    rows = dbCursor.fetchall()
+
+    for row in rows:
+        tree.insert("", "end", values=list(row))  # Используем список вместо кортежа
+
+    connection.close()
+
+    role = 3
+    update_table(tree, new_windowMenager, user_id, role)
+    tree.bind("<Double-1>", lambda event: open_request_window_menager(event, tree))
+
+def get_user_logins_with_role_two():
+    logins = []
+    try:
+        connectionStr = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=.;DATABASE=Account;Trusted_Connection=yes'
+        conn = py.connect(connectionStr)
+        cursor = conn.cursor()
+        cursor.execute("SELECT Login FROM Users WHERE Role = 2")
+        rows = cursor.fetchall()
+        for row in rows:
+            logins.append(row[0])
+        conn.close()
+    except py.Error as ex:
+                messagebox.showerror("Ошибка", f"Ошибка при получении логинов: {ex}")
+    return logins
+
+def open_request_window_menager(event, tree):
+    item = tree.selection()[0]  # Получаем выбранный элемент
+    request_data = tree.item(item, "values")  # Получаем данные о выбранной заявке
+    if request_data:
+        # Создаем новое окно для редактирования заявки
+        edit_windowReq = Tk()
+        edit_windowReq.title("Редактировать заявку")
+        
+        # Функция для обновления описания в базе данных
+        def update_description():
+            id_server = request_data[0]  # Получаем Id_Server
+            new_status = status_var.get()  # Получаем выбранный статус
+            new_equipment = equipment_entry.get()  # Получаем новое название оборудования
+            new_type = type_server_entry.get()  # Получаем новый тип
+            selected_login = user_var.get()  # Получаем выбранный логин
+            try:
+                connectionStr = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=.;DATABASE=Account;Trusted_Connection=yes'
+                conn = py.connect(connectionStr)
+                cursor = conn.cursor()
+                # Обновляем данные в базе данных
+                cursor.execute("UPDATE Servers SET Status_Server = ?, Equipment = ?, Type_Server = ?, Employ_Data = ? WHERE Id_Server = ?", 
+                            (new_status, new_equipment, new_type, selected_login, id_server))
+                conn.commit()  # Применяем изменения
+                conn.close()
+                messagebox.showinfo("Успех", "Данные успешно обновлены.")
+                edit_windowReq.destroy()  # Закрываем окно редактирования после успешного обновления
+            except py.Error as ex:
+                messagebox.showerror("Ошибка", f"Ошибка при обновлении данных: {ex}")
+
+        # Отображаем существующие данные в окне редактирования
+        Label(edit_windowReq, text="ID:").grid(row=0, column=0)
+        Label(edit_windowReq, text=request_data[0]).grid(row=0, column=1)
+        
+        Label(edit_windowReq, text="Дата:").grid(row=1, column=0)
+        Label(edit_windowReq, text=request_data[1]).grid(row=1, column=1)
+        
+        equipment_label = Label(edit_windowReq, text='Название оборудования', font=label_font, **base_padding)
+        equipment_label.grid(row=4, column=0)
+        equipment_entry = Entry(edit_windowReq, bg='#fff',fg='#444', font=font_entry)
+        equipment_entry.insert(0, request_data[2])  # Вставляем существующее название оборудования
+        equipment_entry.grid(row=4, column=1)
+
+        type_label = Label(edit_windowReq, text='Тип', font=label_font, **base_padding)
+        type_label.grid(row=5, column=0)
+        type_server_entry = Entry(edit_windowReq, bg='#fff', fg='#444', font=font_entry)
+        type_server_entry.insert(0, request_data[3])  # Вставляем существующий тип
+        type_server_entry.grid(row=5, column=1)
+
+        status_label = Label(edit_windowReq, text='Статус', font=label_font, **base_padding)
+        status_label.grid(row=6, column=0)
+        status_var = StringVar(edit_windowReq)
+        status_var.set(request_data[6])  # Устанавливаем существующий статус
+        status_dropdown = OptionMenu(edit_windowReq, status_var, "в ожидании", "выполнено", "не выполнено")
+        status_dropdown.grid(row=6, column=1)
+
+        desc_server_label = Label(edit_windowReq, text='Описание:')
+        desc_server_label.grid(row=7, column=0)
+        Label(edit_windowReq, text=request_data[4]).grid(row=7, column=1)
+
+      # Получаем логины пользователей с ролью 2
+        user_logins = get_user_logins_with_role_two()
+
+        # Устанавливаем первый логин по умолчанию
+        user_var = StringVar(edit_windowReq)
+        user_var.set(user_logins[0])
+
+        # Создаем выпадающий список из логинов
+        user_dropdown = OptionMenu(edit_windowReq, user_var, *user_logins)
+        user_dropdown.grid(row=8, column=1)
+
+        # Кнопка для сохранения изменений
+        Button(edit_windowReq, text="Сохранить изменения", command=update_description).grid(row=8, columnspan=2)
+        
+        # Функция для закрытия окна редактирования
+        def close_edit_window():
+            edit_windowReq.destroy()
+        
+        # Кнопка для закрытия окна редактирования
+        Button(edit_windowReq, text="Закрыть", command=close_edit_window).grid(row=9, columnspan=2)
+        
+        # Отображаем окно редактирования
+        edit_windowReq.mainloop()
+    else:
+        messagebox.showinfo("Ошибка", "Не выбрана заявка для редактирования")
+
 def open_employwindow(user_id):
     new_windowEmploy = Tk()
-    new_windowEmploy.title("Окно юзера")
+    new_windowEmploy.title("Окно сотрудника")
     new_windowEmploy.attributes('-fullscreen',True)
     
     # Добавление кнопки создания заявки
@@ -118,8 +269,9 @@ def open_employwindow(user_id):
     tree.column("Equipment", anchor="center", width=150)
     tree.column("Type", anchor="center", width=160)
     tree.column("Description", anchor="center", width=360)
-    tree.column("Client", anchor="center", width=360)
-    tree.column("Status", anchor="center", width=360)
+    tree.column("Client", anchor="center", width=50)
+    tree.column("Status", anchor="center", width=260)
+    tree.column("Employ_Data", anchor="center", width=260)
     
     tree.heading("Id", text="ID")
     tree.heading("Date", text="Дата заявки")
@@ -219,8 +371,7 @@ def open_request_window_employ(event, tree):
         edit_window.mainloop()
     else:
         messagebox.showinfo("Ошибка", "Не выбрана заявка для редактирования")
-
-   
+ 
 
 def update_table(tree, window, user_id, role):
     try:
@@ -325,6 +476,7 @@ def auth():
                 closeWindow(window)
             else:
                 print("Добро пожаловать, {}!".format(username))
+                open_menagerwindow(user[0])
                 closeWindow(window)
         else:
             messagebox.showerror("Ошибка", "Данные не найдены")
